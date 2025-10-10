d08a30502662c0e11f391432e1d3ba853491342f
Lukehowland <lukqs05@gmail.com>
2025-10-10 14:08:32 -0400

    feat: email verification flow complete

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,14 +1,13 @@
 import './bootstrap';
 import { createInertiaApp } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
-import Home from './Pages/Home.tsx';
-
-const pages = {
-    'Home': Home
-};
 
 createInertiaApp({
-    resolve: (name) => pages[name],
+    resolve: (name) => {
+        const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true });
+        const page = pages[`./Pages/${name}.tsx`];
+        return page.default;
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
         root.render(<App {...props} />);
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home', [
         'mensaje' => 'Helpdesk frontend PROXIMAMENTE!!!'
+    ]);
+});
+
+Route::get('/verify-email', function (Request $request) {
+    return Inertia::render('VerifyEmail', [
+        'token' => $request->query('token'),
+        'userId' => $request->query('user_id'),
     ]);
 });
